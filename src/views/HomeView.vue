dbf63508dc6c22f2afcd1b4f9bdb9447c0c00282
Ermek Abdrisaev <abdrisaeve@gmail.com>
2022-10-31 23:57:56 +0600

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>ОТДЕЛЕНИЯ И БАНКОМАТЫ ОПТИМА БАНК</h1>
   </div>
+  <ul>
+    <li>
+      <router-link
+        to="https://www.optimabank.kg/images/files/news/2014/spisok-bankomatov-oao-optima-bank.pdf"
+        >Список Банкоматов Оптима Банк СКАЧАТЬ (pdf)</router-link
+      >
+    </li>
+    <li>
+      <router-link to="https://www.optimabank.kg/images/files/pos_terminals.pdf"
+        >Список POS-терминалов ОАО «Оптима Банк»</router-link
+      >
+    </li>
+    <li>
+      <router-link
+        to="https://www.optimabank.kg/images/files/cashin_terminals_list.pdf"
+        >Список платежных терминалов ОАО «Оптима Банк»</router-link
+      >
+    </li>
+    <li>
+      <router-link
+        to="https://www.optimabank.kg/images/files/news/2015/09/spisok_podrazdeleniy_oao_optima_bank_s_telefonami_i_adresami.pdf"
+        >Справочник подразделений в ОАО «Оптима Банк» (адрес и режим
+        работы)</router-link
+      >
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
@@ -11,8 +36,6 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 });
 </script>
